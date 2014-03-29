d59f9e400cbc48595f64510a72b67c10c8181ed7
Wim Mostmans <wim@sitebase.be>
2014-03-29 22:45:49 +0100

    change motd template path

--- puppet/modules/motd/manifests/init.pp
+++ puppet/modules/motd/manifests/init.pp
@@ -1,7 +1,8 @@
 class motd {
 	
 	file { '/etc/motd':
-    	source => template('motd/motd.erb')
+    	source => puppet:///modules/motd/templates/motd.erb
+    	#source => template('motd/motd.erb')
 	}
 
 }
