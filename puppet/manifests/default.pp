f2199a7fe2ce2361ff9ef7104f0a2401a51320c2
Wim Mostmans <wim@sitebase.be>
2014-03-30 11:12:46 +0200

    make a module of xbmc stuff

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -35,30 +35,7 @@ exec { 'apt-get update2':
 	command => 'apt-get update',
 }
 
-$devPackages2 = [ 'xbmc', 'xbmc-standalone']
-package { $devPackages2:
-	ensure  => 'installed',
-	require => Exec['apt-get update2'],
-}
-
-file { "/etc/init.d/xbmc":
-    path    => "/etc/init.d/xbmc",
-    source => "puppet:///templates/xbmc.init.erb",
-    mode   => "755",
-    owner  => root,
-    group  => root,
-}
-
-user { "xbmc":
-  ensure     => "present",
-  managehome => true,
-}
-
-service { "xbmc":
-	ensure	=> running,
-	enable	=> true,
-	status => '/etc/init.d/xbmc status | grep "is running"',
-}
+include xbmc
 
 #include ssh::server
 include couchpotato 
